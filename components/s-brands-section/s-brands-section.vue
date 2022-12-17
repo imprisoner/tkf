a4d1395b425168f8a9e73c22aae6dadae099c622
Maksim Arkhipov <Max1m.ar@yandex.ru>
2022-12-17 17:16:46 +0300

    add component brandcard

--- components/s-brands-section/s-brands-section.vue
+++ components/s-brands-section/s-brands-section.vue
@@ -3,27 +3,12 @@
     <div class="row">
       <div class="offset-lg-1 col-lg-10 col-12">
         <div class="brands-section__wall row">
-          <div
-            class="card card-square col-lg-3 col-6 brands-logo aspect--1-1"
+          <brands-card
             v-for="brand in brandsCards"
-          >
-            <img
-              class="brands-logo__img img-resp"
-              :src="brand.image || stubBrandImageUrl"
-            />
-            <div class="brands-logo__overlay card card--square">
-              <div
-                class="brands-logo__arrow button button--square button--gray stroked-icon"
-              >
-                <base-icon name="arrow-down-right"></base-icon>
-              </div>
-              <NuxtLink :to="`/brands/${brand.id}`">
-                <h5 class="brands-logo__title">
-                  {{ brand.name }}
-                </h5>
-              </NuxtLink>
-            </div>
-          </div>
+            :image="brand.image"
+            :id="brand.id"
+            :name="brand.name"
+          />
         </div>
       </div>
     </div>
@@ -39,53 +24,6 @@
       default: () => [],
     },
   })
-
-  const stubBrandImageUrl = '/img/brand_stub.png'
 </script>
 
-<style lang="scss" scoped>
-  .brands-logo {
-    &__overlay {
-      position: absolute;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      opacity: 0;
-      transition: opacity 0.8s ease;
-
-      @include max-width('md') {
-        padding: 14px;
-      }
-    }
-
-    &:hover &__img {
-      opacity: 0;
-    }
-
-    &:hover &__overlay {
-      background: black;
-      opacity: 1;
-      z-index: 2;
-    }
-
-    &__title {
-      @include max-width('md') {
-        font-size: 10px;
-        line-height: 12px;
-      }
-    }
-
-    &__arrow {
-      background: rgba(255, 255, 255, 0.45);
-      cursor: default;
-      @include max-width('md') {
-        width: 24px;
-        height: 24px;
-      }
-    }
-  }
-  .card-square {
-    cursor: pointer;
-  }
-</style>
+<style lang="scss" scoped></style>
--- components/s-brands-slider/s-brands-slider.vue
+++ components/s-brands-slider/s-brands-slider.vue
@@ -8,13 +8,23 @@
         <slider-nav></slider-nav>
       </div>
       <div class="offset-xl-1 col-xl-10 col-12">
-        <brands-slider :slides="brandsCards"></brands-slider>
+        <swiper v-bind="config">
+          <swiper-slide v-for="(brand, i) in brandsCards" :key="i">
+            <brands-card
+              :image="brand.image"
+              :id="brand.id"
+              :name="brand.name"
+            />
+          </swiper-slide>
+        </swiper>
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
+  import { Swiper, SwiperSlide } from 'swiper/vue'
+  import { Navigation, FreeMode } from 'swiper'
   import './s-brands-slider.scss'
 
   const props = defineProps({
@@ -23,4 +33,72 @@
       default: () => [],
     },
   })
+
+  const config = {
+    modules: [Navigation, FreeMode],
+    navigation: {
+      prevEl: '.brands-slider__nav .slider-nav__prev',
+      nextEl: '.brands-slider__nav .slider-nav__next',
+    },
+    spaceBetween: 30,
+    slidesPerView: 4,
+    breakpoints: {
+      320: {
+        freeMode: {
+          enabled: true,
+        },
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {},
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+  }
 </script>
+
+<style lang="scss">
+  .brands-logo {
+    &__overlay {
+      position: absolute;
+      top: 0;
+      padding: 36px;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      opacity: 0;
+      transition: opacity 0.8s ease;
+
+      @include max-width('md') {
+        padding: 14px;
+      }
+    }
+
+    &:hover &__img {
+      opacity: 0;
+    }
+
+    &:hover &__overlay {
+      background: black;
+      opacity: 1;
+      z-index: 2;
+    }
+
+    &__title {
+      @include max-width('md') {
+        font-size: 10px;
+        line-height: 12px;
+      }
+    }
+
+    &__arrow {
+      background: rgba(255, 255, 255, 0.45);
+      cursor: default;
+      @include max-width('md') {
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
+</style>
