3b6f7868f9e555ac5cd957997297a143c3984aaf
Maksim Arkhipov <Max1m.ar@yandex.ru>
2022-12-19 16:25:48 +0300

    brand jewerly, watches

---
+++ pages/categories/jewelry/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <main id="categories-page" class="catalog">
+    <SCatalogTop
+      :breadcrumbs="breadcrumbs"
+      :title="title"
+      :btn-show="btnShow"
+      :count="98"
+    />
+    <SCategoriesSection />
+  </main>
+</template>
+
+<script setup>
+  console.log
+  const breadcrumbs = [
+    {
+      text: 'Ювелирные украшения',
+      href: '/categories/jewelry',
+    },
+  ]
+  const title = 'Категории ювелирных украшений'
+  const btnShow = false
+</script>
+
+<style lang="scss" scoped>
+  #categories-page {
+    .s-catalog-top {
+      margin-bottom: 72px;
+    }
+
+    .categories-section {
+      margin-bottom: 260px;
+    }
+
+    @include max-width('md') {
+      .catalog-top {
+        margin-bottom: 24px;
+      }
+
+      .categories-section {
+        margin-bottom: 80px;
+      }
+    }
+  }
+</style>
