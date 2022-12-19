ce65d61e432deae59feae11d7b0dd66f363f6bc6
ogairodion <ogairodion@gmail.com>
2022-12-19 12:31:06 +0300

    error 500

--- api/getBanners.js
+++ api/getBanners.js
@@ -1,8 +1,7 @@
 import useApi from '@/composables/useApi'
 
-export function getBanners({ page , limit } = {}) {
-  const limit = limit ?? 0;
-  return useApi('/banners/', { query: { page, limit }}).then(res => {
+export function getBanners(requestData) {
+  return useApi('/banners', { query: requestData}).then(res => {
     return res.data
   })
 }
