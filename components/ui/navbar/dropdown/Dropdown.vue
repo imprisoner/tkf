ec4ccb72d31cab121aab5e24c8b3c24be930c8c7
max2201 <max22012201@gmail.com>
2022-12-21 23:25:16 +0700

    Fix: fix navbar dropdown close

--- components/ui/navbar/dropdown/Dropdown.vue
+++ components/ui/navbar/dropdown/Dropdown.vue
@@ -15,28 +15,32 @@
       </div>
     </component>
     <template v-if="repository">
-      <div v-show="isActive" class="navbar__menu navbar-menu">
-        <div class="navbar-menu__wrap">
+      <div  v-show="isActive" class="navbar__menu navbar-menu">
+        <div  class="navbar-menu__wrap">
           <div v-if="repository === 'contacts'" class="navbar-menu__contacts">
-            <div
-              v-for="(contactsGroup, contactsGroupName) in contacts"
-              :key="contactsGroupName"
-              class="navbar-menu__contact"
-            >
-              <span class="navbar-menu__contact-title">{{
-                getContactsGroupName(contactsGroupName)
-              }}</span>
-              <a
-                v-for="(contact, index) in contactsGroup"
-                :key="index"
-                :href="setContactLinkByType(contact, contactsGroupName)"
-                target="_blank"
-                >{{ contact }}</a
+            <template v-for="(contactsGroup, contactsGroupName) in contacts"
+                      :key="contactsGroupName">
+              <div
+                v-if="contactsGroup.length"
+                class="navbar-menu__contact"
               >
-            </div>
+              <span class="navbar-menu__contact-title">{{
+                  getContactsGroupName(contactsGroupName)
+                }}</span>
+                <a
+                  v-for="(contact, index) in contactsGroup"
+                  :key="index"
+                  :href="setContactLinkByType(contact, contactsGroupName)"
+                  target="_blank"
+                >{{ contact }}</a
+                >
+              </div>
+
+            </template>
           </div>
           <template v-else>
             <ui-navbar-dropdown-section
+              ref="target"
               v-for="(section, i) in sections"
               :key="i"
               v-bind="section"
@@ -63,228 +67,233 @@
 </template>
 
 <script setup>
-  import { isDesktop } from '@/utils/queries'
+import {isDesktop} from '@/utils/queries'
+import { onClickOutside } from '@vueuse/core'
+import {ref} from "vue";
 
-  const props = defineProps({
-    title: {
-      type: String,
-      default: '',
-    },
-    repository: {
-      type: [String, Object],
-      default: null,
-    },
-    icon: {
-      type: String,
-      default: 'x',
-    },
-    hasDropdown: {
-      type: Boolean,
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      required: true,
-    },
-    link: {
-      type: String,
-      default: '',
-    },
-    categories: {
-      type: Array,
-      default: () => [],
-    },
-    contacts: {
-      type: Array,
-      default: () => [],
-    },
-    brands: {
-      type: Array,
-      default: () => [],
-    },
-  })
+const target = ref(null)
+onClickOutside(target, () => emits('hide'))
 
-  const emits = defineEmits(['show', 'hide'])
+const props = defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  repository: {
+    type: [String, Object],
+    default: null,
+  },
+  icon: {
+    type: String,
+    default: 'x',
+  },
+  hasDropdown: {
+    type: Boolean,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+  link: {
+    type: String,
+    default: '',
+  },
+  categories: {
+    type: Array,
+    default: () => [],
+  },
+  contacts: {
+    type: Array,
+    default: () => [],
+  },
+  brands: {
+    type: Array,
+    default: () => [],
+  },
+})
 
-  const trigger = shallowRef(
-    props.link && !props.hasDropdown ? resolveComponent('NuxtLink') : 'button'
-  )
+const emits = defineEmits(['show', 'hide'])
 
-  const activeClass = computed(() => ({
-    'show-menu': props.isActive,
-  }))
-  const triggerClasses = computed(() => ({
-    'button--black': props.isActive && !isDesktop.value,
-    active: props.isActive && isDesktop.value,
-  }))
+const trigger = shallowRef(
+  props.link && !props.hasDropdown ? resolveComponent('NuxtLink') : 'button'
+)
 
-  const type = props.link ? null : 'button'
+const activeClass = computed(() => ({
+  'show-menu': props.isActive,
+}))
+const triggerClasses = computed(() => ({
+  'button--black': props.isActive && !isDesktop.value,
+  active: props.isActive && isDesktop.value,
+}))
 
-  const bottomLinkIcon = computed(() => {
-    return !isDesktop.value ? 'arrow-right' : 'arrow-down-right'
-  })
+const type = props.link ? null : 'button'
 
-  const bottomLinkClass = computed(() => {
-    return !isDesktop.value ? '' : 'button--black'
-  })
+const bottomLinkIcon = computed(() => {
+  return !isDesktop.value ? 'arrow-right' : 'arrow-down-right'
+})
 
-  const params = reactive({
-    categories: {
-      watches: {},
-      jewelry: {},
+const bottomLinkClass = computed(() => {
+  return !isDesktop.value ? '' : 'button--black'
+})
+
+const params = reactive({
+  categories: {
+    watches: {},
+    jewelry: {},
+  },
+  brands: {
+    watches: {
+      brand_type: 'WATCH',
     },
-    brands: {
-      watches: {
-        brand_type: 'WATCH',
-      },
-      jewelry: {
-        brand_type: 'JEWELRY',
-      },
+    jewelry: {
+      brand_type: 'JEWELRY',
     },
-  })
+  },
+})
 
-  const sections = reactive([
-    {
-      name: 'categories',
-      title: 'Категории',
-      repository: props.repository,
-      list: props.categories,
-    },
-    {
-      name: 'brands',
-      title: 'Бренды',
-      repository: props.repository,
-      list: props.brands,
-    },
-  ])
+const sections = reactive([
+  {
+    name: 'categories',
+    title: 'Категории',
+    repository: props.repository,
+    list: props.categories,
+  },
+  {
+    name: 'brands',
+    title: 'Бренды',
+    repository: props.repository,
+    list: props.brands,
+  },
+])
 
-  function setContactLinkByType(value, contactsGroupName) {
-    const contactTypesMap = {
-      phones: (val) => `tel:${val}`,
-      mails: (val) => `mailto:${val}`,
-    }
-    return contactTypesMap[contactsGroupName]?.(value) || value
+function setContactLinkByType(value, contactsGroupName) {
+  const contactTypesMap = {
+    phones: (val) => `tel:${val}`,
+    email: (val) => `mailto:${val}`,
+  }
+  return contactTypesMap[contactsGroupName]?.(value) || value
+}
+
+function getContactsGroupName(contactsGroupName) {
+  const nameMap = {
+    phones: 'Телефон',
+    socials: 'Соц. сети',
+    email: 'E-mail',
+  }
+  return nameMap[contactsGroupName] || contactsGroupName
+}
+
+function onClick() {
+  if (!props.hasDropdown) {
+    return
   }
 
-  function getContactsGroupName(contactsGroupName) {
-    const nameMap = {
-      phones: 'Телефон',
-      socials: 'Соц. сети',
-      mails: 'Почта',
-    }
-    return nameMap[contactsGroupName] || contactsGroupName
-  }
-
-  function onClick() {
-    if (!props.hasDropdown) {
-      return
-    }
-
-    emits('show')
-  }
+  emits('show')
+}
 </script>
 
 <style lang="scss" scoped>
-  .navbar__dropdown {
-    position: relative;
+.navbar__dropdown {
+  position: relative;
 
-    @include max-width('lg') {
-      width: 100%;
+  @include max-width('lg') {
+    width: 100%;
+  }
+}
+
+.navbar__dropdown-trigger {
+  @include max-width('lg') {
+    width: 100%;
+    justify-content: start;
+    border-bottom: $border;
+    line-height: 16px;
+    height: unset;
+    font-size: 12px;
+    padding: 12px 14px;
+  }
+
+  &.active {
+    background-color: #ffffffe5;
+  }
+
+  &.active .button__caret {
+    transform: rotate(180deg);
+  }
+}
+
+.navbar-menu {
+  position: absolute;
+  top: 84px;
+  left: 0;
+  padding-bottom: unset;
+  background-color: white;
+  @include shadow;
+
+  &__contacts {
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+  }
+
+  &__contact {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    &-title {
+      font-weight: 600;
+      font-size: 20px;
+      line-height: 24px;
+    }
+
+    &-value {
+      line-height: 26px;
     }
   }
 
-  .navbar__dropdown-trigger {
-    @include max-width('lg') {
-      width: 100%;
-      justify-content: start;
-      border-bottom: $border;
-      line-height: 16px;
-      height: unset;
-      font-size: 12px;
-      padding: 12px 14px;
-    }
-
-    &.active {
-      background-color: #ffffffe5;
-    }
-
-    &.active .button__caret {
-      transform: rotate(180deg);
-    }
+  @include max-width('lg') {
+    position: static;
+    width: 100%;
+    border: none;
+    padding: unset;
+    box-shadow: none;
+    display: flex;
+    flex-direction: column;
   }
 
-  .navbar-menu {
-    position: absolute;
-    top: 84px;
-    left: 0;
-    padding-bottom: unset;
-    background-color: white;
-    @include shadow;
-
-    &__contacts {
-      display: flex;
-      flex-direction: column;
-      gap: 32px;
-    }
-
-    &__contact {
-      display: flex;
-      flex-direction: column;
-      gap: 16px;
-
-      &-title {
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-      }
-
-      &-value {
-        line-height: 26px;
-      }
-    }
+  &__wrap {
+    display: grid;
+    grid-auto-flow: column;
+    gap: 40px;
+    height: 100%;
+    padding: 40px;
+    padding-bottom: 45px;
 
     @include max-width('lg') {
-      position: static;
-      width: 100%;
-      border: none;
+      display: flex;
+      flex-direction: column;
       padding: unset;
-      box-shadow: none;
-      display: flex;
-      flex-direction: column;
-    }
-
-    &__wrap {
-      display: grid;
-      grid-auto-flow: column;
-      gap: 40px;
-      height: 100%;
-      padding: 40px;
-      padding-bottom: 45px;
-
-      @include max-width('lg') {
-        display: flex;
-        flex-direction: column;
-        padding: unset;
-        gap: unset;
-        order: 1;
-      }
+      gap: unset;
+      order: 1;
     }
   }
+}
 
-  .bottom-link {
+.bottom-link {
+  @include max-width('lg') {
+    border-bottom: $border;
+    justify-content: flex-start;
+    font-weight: 400;
+  }
+
+  &__icon {
     @include max-width('lg') {
-      border-bottom: $border;
-      justify-content: flex-start;
-      font-weight: 400;
-    }
-
-    &__icon {
-      @include max-width('lg') {
-        order: 1;
-        width: 16px;
-        height: 16px;
-        margin-left: auto;
-      }
+      order: 1;
+      width: 16px;
+      height: 16px;
+      margin-left: auto;
     }
   }
+}
 </style>
