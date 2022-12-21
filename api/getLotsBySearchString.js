970b628aff9cf41eda27f8411bb3082b0aea9868
alex_kudryashov <kudryashov_2017@bk.ru>
2022-12-21 20:50:31 +0300

    pagination and sort trash code corrected

--- api/getLotsBySearchString.js
+++ api/getLotsBySearchString.js
@@ -1,8 +1,9 @@
 import useApi from '@/composables/useApi'
-
 export function getLotsBySearchString(params) {
   return useApi('/lots/search/', {
     query: {
+      limit: 30,
+      ordering: 'update_dt',
       ...params,
     },
   })
--- composables/usePagination.js
+++ composables/usePagination.js
@@ -4,19 +4,8 @@ export default function (commonCount) {
   const { getUrlSearchParams, setUrlSearchParams } = useQueryString()
   const limits = [30, 60, 120]
   const currentPage = ref(1)
-  const getOffset = computed(
-    () => +getUrlSearchParams.value.limit * (currentPage.value - 1)
-  )
+  const getOffset = computed(() => perPageLimit.value * (currentPage.value - 1))
 
-  const setDefaults = () => {
-    if (!getUrlSearchParams.value.limit || !getUrlSearchParams.value.offset) {
-      setUrlSearchParams({ limit: limits[0], offset: 0 })
-    }
-  }
-
-  setDefaults()
-
-  watch(getUrlSearchParams, setDefaults)
   watch(
     computed(() => currentPage.value),
     () => setUrlSearchParams({ offset: getOffset.value })
