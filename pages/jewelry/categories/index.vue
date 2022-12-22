b0ded140a9623627eafee15cbcc0b1fc34f948b4
Bochkarev Ivan <ivanx86@gmail.com>
2022-12-23 01:21:53 +0600

    Update component SCategoriesSection

--- pages/jewelry/categories/index.vue
+++ pages/jewelry/categories/index.vue
@@ -2,20 +2,51 @@
   <main id="categories-page" class="catalog">
     <SCatalogTop
       :title="title"
-      :btn-show="btnShow"
+      :btn-show="false"
       :on-show-counter="false"
       :on-show-categories="false"
       :on-show-bookmark="false"
     />
-    <SCategoriesSection :categories-items="categoriesItems" />
+    <SCategoriesSection
+      :hardcode-items="cardsItems"
+      :categories-items="categoriesItems"
+      :stub-brand-image-url="stubBrandImageUrl"
+    />
   </main>
 </template>
 
 <script setup>
   import { getCategories } from '@/api/getCategories'
-  const categoriesItems = await getCategories()
+
+  const categoriesItems = await getCategories({ inComponent: false })
   const title = 'Категории ювелирных украшений'
-  const btnShow = false
+  const stubBrandImageUrl = '/img/stub_jewelry.jpg'
+  const cardsItems = [
+    {
+      name: 'Мужские',
+      classname: 'col-lg-3 col-md-6 col-12',
+      link: '/watches/',
+      image: '/img/stub_jewelry.jpg',
+    },
+    {
+      name: 'Женские',
+      classname: 'col-lg-3 col-md-6 col-12',
+      link: '/watches/',
+      image: '/img/stub_jewelry.jpg',
+    },
+    {
+      name: 'Новые',
+      classname: 'col-lg-3 col-md-6 col-12',
+      link: '/watches/',
+      image: '/img/stub_jewelry.jpg',
+    },
+    {
+      name: 'Подержанные',
+      classname: 'col-lg-3 col-md-6 col-12',
+      link: '/watches/',
+      image: '/img/stub_jewelry.jpg',
+    },
+  ]
 </script>
 
 <style lang="scss" scoped>
