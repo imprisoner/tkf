8b6327870b15b4d4d450b9c5cee41255782e3479
Maksim Arkhipov <Max1m.ar@yandex.ru>
2022-12-25 17:37:56 +0300

    fix range slider

--- components/m-range-slider/m-range-slider.vue
+++ components/m-range-slider/m-range-slider.vue
@@ -1,47 +1,41 @@
 <template>
   <div class="range-slider">
-    <div
-      class="range-slider__wrap"
-      style="`--a: ${a}; --b: ${b}; --min: ${min}; --max: ${max}`"
-    >
+    <div class="range-slider__wrap">
       <label for="a" class="range-slider__label">0 USD</label>
-      <input id="a" type="range" min="min" value="a" max="max" />
-      <output
-        class="range-slider__top-label"
-        for="a"
-        style="--c: var(--a); left: 15%"
-      ></output>
-      <output
-        class="range-slider__bottom-label"
-        for="a"
-        style="--c: var(--a); left: 15%"
-      ></output>
+      <Slider v-model="value" :format="format" />
       <label for="b" class="range-slider__label">15 500 0000 USD</label>
-      <input id="b" type="range" min="min" value="b" max="max" />
-      <output
-        class="range-slider__top-label"
-        for="b"
-        style="--c: var(--b); right: 20%"
-      ></output>
-      <output
-        class="range-slider__bottom-label"
-        for="b"
-        style="--c: var(--b); right: 20%"
-      ></output>
-      <div class="range-slider__track"></div>
     </div>
   </div>
 </template>
 
 <script setup>
   import './m-range-slider.scss'
+  import Slider from '@vueform/slider'
+  import '@vueform/slider/themes/default.css'
 
-  // addEventListener(
-  //   'input',
-  //   (e) => {
-  //     let _t = e.target
-  //     _t.parentNode.style.setProperty(`--${_t.id}`, +_t.value)
-  //   },
-  //   false
-  // )
+  const value = [25, 75]
+
+  function format(value) {
+    return value.toFixed(2)
+  }
 </script>
+
+<style lang="scss">
+  .slider-horizontal .slider-tooltip-top:before {
+    display: none;
+  }
+
+  .slider-connect {
+    background: #000000;
+  }
+
+  .slider-tooltip {
+    color: #6e6b7b;
+    background: #f8f8f8;
+    border-color: #f8f8f8;
+  }
+
+  .slider-handle {
+    border: 2px solid #82868b;
+  }
+</style>
