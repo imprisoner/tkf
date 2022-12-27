c26848844f45752a8577d3f55efac78e48b2f90f
max2201 <max22012201@gmail.com>
2022-12-27 17:44:13 +0700

    Fix: fix diameter filter

--- api/getFilterObject.js
+++ api/getFilterObject.js
@@ -1,6 +1,6 @@
 import useApi from '@/composables/useApi'
 export function getFilterObject(slug) {
-  return useApi(`/lots/${slug}/filter-objects`, {
+  return useApi(`/lots/${slug}/filter-objects/`, {
     query: {},
   })
 }
