88b2ced328cd796704bc80fb30afb2b5494a8124
Bochkarev Ivan <ivanx86@gmail.com>
2022-12-29 13:07:51 +0600

    Move code to component

---
+++ components/s-good-section-empty/s-good-section-empty.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="goods-section container">
+    <div class="row">
+      <div class="offset-lg-1 col-lg-10 col-12">
+        <div class="empty-block">
+          <div class="empty-block__image">
+            <base-icon name="crystal_xl"></base-icon>
+          </div>
+          <div class="empty-block__title">Пусто</div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+  import './s-good-section-empty.scss'
+</script>
--- pages/watches/index.vue
+++ pages/watches/index.vue
@@ -14,18 +14,7 @@
       good-type="watches"
     />
 
-    <section v-else class="goods-section container">
-      <div class="row">
-        <div class="offset-lg-1 col-lg-10 col-12">
-          <div class="empty-block">
-            <div class="empty-block__image">
-              <base-icon name="crystal_xl"></base-icon>
-            </div>
-            <div class="empty-block__title">Пусто</div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <SGoodSectionEmpty v-else/>
 
     <SAdsSection :banners="banners" />
 
