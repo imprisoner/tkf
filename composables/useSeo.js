3fa156b10ab232cd7ea16517ffae66391542fda8
max2201 <max22012201@gmail.com>
2022-12-29 20:31:29 +0700

    Fix: fix error 500

--- composables/useSeo.js
+++ composables/useSeo.js
@@ -4,7 +4,7 @@ import { getLotDynamicSeo } from "~/utils/seo";
 export default function (routeName,options) {
   const { public: { urlBase: baseUrl } } = useRuntimeConfig()
   const tags = options ? getLotDynamicSeo({...options}) : META_TAGS[routeName]
-  if (!options) {
+  if (tags && !options) {
     tags.meta.find(tag=>tag.name==='og:image').content = `${baseUrl}${tags.meta.find(tag=>tag.name==='og:image').content}`
   }
 
